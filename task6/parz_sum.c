4fbd8295bfb7754fa2c7b622cc7238224f850144
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-06 14:50:03 +0400

    tnayin

--- task6/parz_sum.c
+++ task6/parz_sum.c
@@ -1,6 +1,11 @@
 #include "main.c"
 
 int parz_sum(int num){
+	for(int i = 0; i < 4; ++i){
+		if(num == i){
+			return 0;
+		}
+	}
 	for(int i = 1; i < num; ++i){
 		if(num == 11 || num == 11 + 6 * i){
 			return 0;
